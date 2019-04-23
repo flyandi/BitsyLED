bd41dda8d25db2887cc97a5b295db5a80d728d78
Andy <flyandi@yahoo.com>
2019-04-22 21:21:08 -0700

    adds build process

--- configurator/src/index.js
+++ configurator/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Loader from './Loader';
+const { autoUpdater } = require("electron-updater");
 
+autoUpdater.checkForUpdatesAndNotify();
 ReactDOM.render(<Loader />, document.getElementById('root'));
